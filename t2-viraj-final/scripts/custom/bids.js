88b00bab4eee3728895b9d34b0948a58ad143424
unknown <abhivispute33@gmail.com>
2020-01-24 11:29:06 +0530

    Internal Hackathon

---
+++ t2-viraj-final/scripts/custom/bids.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $(window).on("load", function() {
+    $("#bidModal").modal("show");
+    alert("jquery");
+  });
+});
---
+++ t2-viraj-final/scripts/custom/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#deployed").click(function() {
+    window.location.href = "./deployed.html";
+  });
+  $("#manager").click(function() {
+    window.location.href = "./manager.html";
+  });
+  $("#bids").click(function() {
+    window.location.href = "./bids.html";
+  });
+});
